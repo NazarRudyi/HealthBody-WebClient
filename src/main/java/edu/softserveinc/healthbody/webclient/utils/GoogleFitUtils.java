7943d95489b7ed4e90c77bdb37c4de19dea4579b
Pavlo Fedoryshyn <fedorushunp@gmail.com>
2016-09-08 15:45:23 +0300

    users score updating method changed

--- src/main/java/edu/softserveinc/healthbody/webclient/utils/GoogleFitUtils.java
+++ src/main/java/edu/softserveinc/healthbody/webclient/utils/GoogleFitUtils.java
@@ -7,7 +7,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -130,10 +129,8 @@ public class GoogleFitUtils {
 	}
 
 	public static void updateUsersScoresInCompetition(HealthBodyService service) {
-		List<UserDTO> users = service.getAllUsers(0, 0);
-		for (UserDTO userDTO : users) {
-			List<CompetitionDTO> competitions = service.getAllActiveCompetitionsByUser(0, 0, userDTO.getLogin());
-			for (CompetitionDTO competitionDTO : competitions) {
+		for (UserDTO userDTO : service.getAllUsers(0, 0)) {
+			for (CompetitionDTO competitionDTO : service.getAllActiveCompetitionsByUser(0, 0, userDTO.getLogin())) {
 				String gettedAccessToken = GoogleFitUtils
 						.postForAccessToken(service.getUserByLogin(userDTO.getLogin()).getGoogleApi());
 				Long startTime = CustomDateFormater.getDateInMilliseconds(
